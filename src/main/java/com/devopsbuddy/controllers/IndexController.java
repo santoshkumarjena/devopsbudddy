241f53ee4bd02763dc4c93bc796b3c9edc9513bf
santoshkumarjena <santoshkumarjena@gmail.com>
2017-12-10 23:39:10 +0530

    added index.html placeholder

--- src/main/java/com/devopsbuddy/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/controllers/IndexController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;;
 
 @Controller
-public class HelloWorldController {
-	
+public class IndexController {
 	@RequestMapping("/")
 	public String sayHello(){
 		return "index";
 	}
-
 }

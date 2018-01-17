d44eb63e51cc7a455242c94d4c207b7bb5d7ca39
santoshkumarjena <santoshkumarjena@gmail.com>
2018-01-18 00:54:52 +0530

    login branch chanegs

--- src/main/java/com/devopsbuddy/web/controllers/ContactController.java
+++ src/main/java/com/devopsbuddy/web/controllers/ContactController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.controllers;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
